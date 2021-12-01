7302c037597e3728d1b8ff130a5516a5f86b3e44
Andoree <and2900@inbox.ru>
2021-12-01 18:16:02 +0300

    UMLS filtering script job name change

--- scripts/filter_umls.sh
+++ scripts/filter_umls.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=train_att          # Название задачи
+#SBATCH --job-name=job          # Название задачи
 #SBATCH --error=delete.err        # Файл для вывода ошибок
 #SBATCH --output=delete.txt       # Файл для вывода результатов
 #SBATCH --time=00:05:00                      # Максимальное время выполнения
