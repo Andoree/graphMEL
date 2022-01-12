310d7cc7cd12a02d416cdf0bd4cbda737aea08b8
An <and2900@inbox.ru>
2022-01-12 19:20:48 +0300

    Mrconso statistics script

---
+++ scripts/preprocessing/count_mrconso_stats.py
@@ -0,0 +1,25 @@
+import codecs
+import os.path
+from argparse import ArgumentParser
+from ..utils import read_mrrel, read_mrconso
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('--mrconso')
+    parser.add_argument('--output_path')
+    args = parser.parse_args()
+    output_path = args.output_path
+    output_dir = os.path.dirname(output_path)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    df_mrconso = read_mrconso(args.mrconso)
+    with codecs.open(output_path, 'w+', encoding="utf-8") as out_file:
+        for col in df_mrconso.columns:
+            # print(col, ' : ', len(df_mrconso[col].unique()))
+            out_file.write(f"{col} : {len(df_mrconso[col].unique())}")
+
+
+if __name__ == '__main__':
+    main()
