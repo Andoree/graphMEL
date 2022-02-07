434382f64dd93b72a25b5603812d247a68a7d844
Andoree <and2900@inbox.ru>
2022-02-07 22:18:07 +0300

    Embedding inference: added nan handling

--- scripts/preprocessing/get_concept_embeddings.py
+++ scripts/preprocessing/get_concept_embeddings.py
@@ -34,7 +34,7 @@ def get_concept_embeddings(concepts, model, tokenizer, device):
 class MrconsoConceptDataset(Dataset):
     def __init__(self, mrconso_df):
         self.mrconso = mrconso_df
-        self.concepts = mrconso_df["STR"].values
+        self.concepts = mrconso_df["STR"].fillna('').values
 
     def __getitem__(self, idx):
         return self.concepts[idx]
