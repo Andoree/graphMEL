d5da9d9f99a8e55352723395ab5d361b4831364a
Andoree <and2900@inbox.ru>
2022-02-09 01:45:21 +0300

    fixed read_csv quoting for MRCONSO and MRSTY

--- scripts/preprocessing/utils/read_umls.py
+++ scripts/preprocessing/utils/read_umls.py
@@ -4,12 +4,12 @@ import pandas as pd
 def read_mrconso(fpath):
     columns = ['CUI', 'LAT', 'TS', 'LUI', 'STT', 'SUI', 'ISPREF', 'AUI', 'SAUI', 'SCUI', 'SDUI', 'SAB', 'TTY', 'CODE',
                'STR', 'SRL', 'SUPPRESS', 'CVF', 'NOCOL']
-    return pd.read_csv(fpath, names=columns, sep='|', encoding='utf-8')
+    return pd.read_csv(fpath, names=columns, sep='|', encoding='utf-8', quoting=3)
 
 
 def read_mrsty(fpath):
     columns = ['CUI', 'TUI', 'STN', 'STY', 'ATUI', 'CVF', 'NOCOL']
-    return pd.read_csv(fpath, names=columns, sep='|', encoding='utf-8')
+    return pd.read_csv(fpath, names=columns, sep='|', encoding='utf-8', quoting=3)
 
 
 def read_mrrel(fpath):
