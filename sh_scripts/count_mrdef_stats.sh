399b1d02d8af531dfa628f72ae6614476da28900
Andoree <and2900@inbox.ru>
2022-03-18 04:24:02 +0300

    Fixed stats sh script

--- sh_scripts/count_mrdef_stats.sh
+++ sh_scripts/count_mrdef_stats.sh
@@ -5,8 +5,8 @@
 #SBATCH --time=00:20:00                      # Максимальное время выполнения
 #SBATCH --cpus-per-task=1                   # Количество CPU на одну задачу
 
-python ../scripts/statistics/count_concept_descriptions.py --mrconso "../UMLS/2020AB/MRCONSO.RRF" \
+python ../scripts/statistics/count_concept_descriptions.py --mrconso "../UMLS/2020AB/MSH_MDR_SNOMEDCT_US_ICD10CM_ICD9CM_ICD10_DRUGBANK_RXNORM_ENG_FRE_GER_SPA_DUT_MRCONSO_filt.RRF" \
 --groupby_sab \
 --mrdef "../UMLS/2020AB/MRDEF.RRF" \
---groupby_stats_output_path "../statistics/stats_2020AB.txt" \
+--groupby_stats_output_path "../statistics/stats_bysab_2020AB.tsv" \
 --global_stats_output_path "../statistics/stats_2020AB.txt"
