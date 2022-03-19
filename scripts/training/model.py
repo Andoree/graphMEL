976d218bf9c5ae13dccd69f7ce7d173ad775eff4
Andoree <and2900@inbox.ru>
2022-03-19 03:21:17 +0300

    SageConv multi-gpu

--- scripts/training/model.py
+++ scripts/training/model.py
@@ -2,6 +2,17 @@ import torch
 from torch import nn as nn
 from torch.nn import functional as F
 from torch_geometric.nn import SAGEConv
+from torch_geometric.utils.num_nodes import maybe_num_nodes
+from torch_sparse import SparseTensor
+from torch.utils.data import DataLoader
+
+try:
+    import torch_cluster
+
+    random_walk = torch.ops.torch_cluster.random_walk
+except ImportError:
+    random_walk = None
+EPS = 1e-15
 
 
 class GraphSAGEOverBert(nn.Module):
@@ -11,15 +22,19 @@ class GraphSAGEOverBert(nn.Module):
         self.convs = nn.ModuleList()
         if multigpu_flag:
             self.bert_encoder = nn.DataParallel(bert_encoder)
+            for i in range(num_layers):
+                in_channels = self.bert_hidden_dim if i == 0 else hidden_channels
+                self.convs.append(nn.DataParallel(SAGEConv(in_channels, hidden_channels)))
         else:
             self.bert_encoder = bert_encoder
+            for i in range(num_layers):
+                in_channels = self.bert_hidden_dim if i == 0 else hidden_channels
+                self.convs.append(SAGEConv(in_channels, hidden_channels))
         self.bert_encoder = bert_encoder
         self.bert_hidden_dim = bert_encoder.config.hidden_size
         self.graphsage_dropout = graphsage_dropout
 
-        for i in range(num_layers):
-            in_channels = self.bert_hidden_dim if i == 0 else hidden_channels
-            self.convs.append(SAGEConv(in_channels, hidden_channels))
+
 
     def forward(self, input_ids, attention_mask, adjs):
         last_hidden_states = self.bert_encoder(input_ids, attention_mask=attention_mask,
@@ -40,3 +55,116 @@ class GraphSAGEOverBert(nn.Module):
                 x = x.relu()
                 x = F.dropout(x, p=self.graphsage_dropout, training=self.training)
         return x
+
+
+class Node2Vec(torch.nn.Module):
+
+    def __init__(self, bert_encoder, edge_index, embedding_dim, walk_length, context_size,
+                 walks_per_node=1, p=1, q=1, num_negative_samples=1,
+                 num_nodes=None, sparse=False):
+        super().__init__()
+
+        if random_walk is None:
+            raise ImportError('`Node2Vec` requires `torch-cluster`.')
+
+        N = maybe_num_nodes(edge_index, num_nodes)
+        row, col = edge_index
+        self.adj = SparseTensor(row=row, col=col, sparse_sizes=(N, N))
+        self.adj = self.adj.to('cpu')
+
+        assert walk_length >= context_size
+
+        self.embedding_dim = embedding_dim
+        self.walk_length = walk_length - 1
+        self.context_size = context_size
+        self.walks_per_node = walks_per_node
+        self.p = p
+        self.q = q
+        self.num_negative_samples = num_negative_samples
+        self.bert_encoder = bert_encoder
+        # self.embedding = Embedding(N, embedding_dim, sparse=sparse)
+
+        self.reset_parameters()
+
+    def forward(self, batch=None):
+        """Returns the embeddings for the nodes in :obj:`batch`."""
+        emb = self.embedding.weight
+        return emb if batch is None else emb.index_select(0, batch)
+
+    def loader(self, **kwargs):
+        return DataLoader(range(self.adj.sparse_size(0)),
+                          collate_fn=self.sample, **kwargs)
+
+    def pos_sample(self, batch):
+        batch = batch.repeat(self.walks_per_node)
+        rowptr, col, _ = self.adj.csr()
+        rw = random_walk(rowptr, col, batch, self.walk_length, self.p, self.q)
+        if not isinstance(rw, torch.Tensor):
+            rw = rw[0]
+
+        walks = []
+        num_walks_per_rw = 1 + self.walk_length + 1 - self.context_size
+        for j in range(num_walks_per_rw):
+            walks.append(rw[:, j:j + self.context_size])
+        return torch.cat(walks, dim=0)
+
+    def neg_sample(self, batch):
+        batch = batch.repeat(self.walks_per_node * self.num_negative_samples)
+
+        rw = torch.randint(self.adj.sparse_size(0),
+                           (batch.size(0), self.walk_length))
+        rw = torch.cat([batch.view(-1, 1), rw], dim=-1)
+
+        walks = []
+        num_walks_per_rw = 1 + self.walk_length + 1 - self.context_size
+        for j in range(num_walks_per_rw):
+            walks.append(rw[:, j:j + self.context_size])
+        return torch.cat(walks, dim=0)
+
+    def sample(self, batch):
+        if not isinstance(batch, torch.Tensor):
+            batch = torch.tensor(batch)
+        return self.pos_sample(batch), self.neg_sample(batch)
+
+    def loss(self, pos_rw, neg_rw):
+        r"""Computes the loss given positive and negative random walks."""
+
+        # Positive loss.
+        start, rest = pos_rw[:, 0], pos_rw[:, 1:].contiguous()
+
+        h_start = self.embedding(start).view(pos_rw.size(0), 1,
+                                             self.embedding_dim)
+        h_rest = self.embedding(rest.view(-1)).view(pos_rw.size(0), -1,
+                                                    self.embedding_dim)
+
+        out = (h_start * h_rest).sum(dim=-1).view(-1)
+        pos_loss = -torch.log(torch.sigmoid(out) + EPS).mean()
+
+        # Negative loss.
+        start, rest = neg_rw[:, 0], neg_rw[:, 1:].contiguous()
+
+        h_start = self.embedding(start).view(neg_rw.size(0), 1,
+                                             self.embedding_dim)
+        h_rest = self.embedding(rest.view(-1)).view(neg_rw.size(0), -1,
+                                                    self.embedding_dim)
+
+        out = (h_start * h_rest).sum(dim=-1).view(-1)
+        neg_loss = -torch.log(1 - torch.sigmoid(out) + EPS).mean()
+
+        return pos_loss + neg_loss
+
+    def test(self, train_z, train_y, test_z, test_y, solver='lbfgs',
+             multi_class='auto', *args, **kwargs):
+        r"""Evaluates latent space quality via a logistic regression downstream
+        task."""
+        from sklearn.linear_model import LogisticRegression
+
+        clf = LogisticRegression(solver=solver, multi_class=multi_class, *args,
+                                 **kwargs).fit(train_z.detach().cpu().numpy(),
+                                               train_y.detach().cpu().numpy())
+        return clf.score(test_z.detach().cpu().numpy(),
+                         test_y.detach().cpu().numpy())
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}({self.embedding.weight.size(0)}, '
+                f'{self.embedding.weight.size(1)})')
