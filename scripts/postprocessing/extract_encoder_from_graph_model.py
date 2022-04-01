099bae490eeff78a88929554584e67558593b004
Andoree <and2900@inbox.ru>
2022-04-01 14:12:39 +0300

    Removed redundant code in encoder extraction script

--- scripts/postprocessing/extract_encoder_from_graph_model.py
+++ scripts/postprocessing/extract_encoder_from_graph_model.py
@@ -5,7 +5,6 @@ from argparse import ArgumentParser
 import torch
 from transformers import AutoTokenizer, AutoModel
 
-from graphmel.scripts.training.model import GraphSAGEOverBert, BertOverNode2Vec
 from graphmel.scripts.utils.io import load_dict, save_encoder_from_checkpoint
 
 
@@ -16,22 +15,8 @@ def main(args):
     
     tokenizer = AutoTokenizer.from_pretrained(model_parameters_dict["text_encoder"])
     bert_encoder = AutoModel.from_pretrained(model_parameters_dict["text_encoder"]).to(device)
-    print(dir(model_parameters_dict["text_encoder"]))
-    multigpu_flag = False
-    if args.graph_network_architecture == "graphsage":
-        num_layers = int(model_parameters_dict["graphsage_num_layers"])
-        hidden_channels = int(model_parameters_dict["graphsage_num_channels"])
-        graphsage_dropout = float(model_parameters_dict["graphsage_dropout"])
-        model = GraphSAGEOverBert(bert_encoder=bert_encoder, hidden_channels=hidden_channels, num_layers=num_layers,
-                                  graphsage_dropout=graphsage_dropout, multigpu_flag=multigpu_flag)
-    elif args.graph_network_architecture == "node2vec":
-        seq_max_length = 32
-        model = BertOverNode2Vec(bert_encoder=bert_encoder, seq_max_length=seq_max_length, multigpu_flag=multigpu_flag)
-    else:
-        raise NotImplementedError(f"Graph architecture {args.graph_network_architecture} is not supported")
 
     checkpoint = torch.load(args.model_checkpoint_path)
-    # print('aaaaa', checkpoint["model_state"])
     bert_encoder.load_state_dict(checkpoint["model_state"])
 
     save_encoder_from_checkpoint(bert_encoder=bert_encoder, bert_tokenizer=tokenizer, save_path=args.output_dir)
@@ -41,7 +26,6 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S', )
     parser = ArgumentParser()
-    parser.add_argument('--graph_network_architecture', type=str)
     parser.add_argument('--model_checkpoint_path', type=str)
     parser.add_argument('--graph_model_dir', type=str)
     parser.add_argument('--output_dir', type=str)
