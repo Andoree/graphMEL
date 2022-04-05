cb53efcfb276d5253e217a06a63b2f82251ff452
Andoree <and2900@inbox.ru>
2022-04-05 19:06:48 +0300

    Modified Bert encoder extraction script

--- scripts/postprocessing/extract_encoder_from_graph_model.py
+++ scripts/postprocessing/extract_encoder_from_graph_model.py
@@ -8,26 +8,48 @@ from transformers import AutoTokenizer, AutoModel
 from graphmel.scripts.utils.io import load_dict, save_encoder_from_checkpoint
 
 
-def main(args):
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model_description_path = os.path.join(args.graph_model_dir, "model_description.tsv")
+def save_bert_encoder_from_graph_model(pretrained_graph_model_dir: str, bert_initialization_model: str,
+                                       checkpoint_name: str, output_dir: str, device: str):
+    model_description_path = os.path.join(pretrained_graph_model_dir, "model_description.tsv")
+    model_checkpoint_path = os.path.join(pretrained_graph_model_dir, checkpoint_name)
     model_parameters_dict = load_dict(path=model_description_path, )
-    
-    tokenizer = AutoTokenizer.from_pretrained(model_parameters_dict["text_encoder"])
-    bert_encoder = AutoModel.from_pretrained(model_parameters_dict["text_encoder"]).to(device)
+    pretrained_model_encoder_path_or_name = os.path.basename(model_parameters_dict["text_encoder"])
+    if pretrained_model_encoder_path_or_name not in bert_initialization_model:
+        raise ValueError(f"Pretrained graph-based encoder does not match with initialized model: "
+                         f"{pretrained_model_encoder_path_or_name} not in {bert_initialization_model}")
 
-    checkpoint = torch.load(args.model_checkpoint_path)
+    tokenizer = AutoTokenizer.from_pretrained(bert_initialization_model)
+    bert_encoder = AutoModel.from_pretrained(bert_initialization_model).to(device)
+
+    checkpoint = torch.load(model_checkpoint_path)
     bert_encoder.load_state_dict(checkpoint["model_state"])
 
-    save_encoder_from_checkpoint(bert_encoder=bert_encoder, bert_tokenizer=tokenizer, save_path=args.output_dir)
+    save_encoder_from_checkpoint(bert_encoder=bert_encoder, bert_tokenizer=tokenizer, save_path=output_dir)
+
+
+def main(args):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    for model_dir in os.listdir(args.input_pretrained_graph_models_dir):
+        input_pretrained_graph_model_dir = os.path.join(args.input_pretrained_graph_models_dir, model_dir)
+        for file in os.listdir(input_pretrained_graph_model_dir):
+            if file.endswith('pth'):
+                output_pretrained_bert_dir = os.path.join(args.output_dir, model_dir, f"{file}/")
+                checkpoint_name = file
+                if not os.path.exists(output_pretrained_bert_dir):
+                    os.makedirs(output_pretrained_bert_dir)
+
+                save_bert_encoder_from_graph_model(pretrained_graph_model_dir=input_pretrained_graph_model_dir,
+                                                   bert_initialization_model=args.bert_initialization_model,
+                                                   checkpoint_name=checkpoint_name,
+                                                   output_dir=output_pretrained_bert_dir, device=device)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S', )
     parser = ArgumentParser()
-    parser.add_argument('--model_checkpoint_path', type=str)
-    parser.add_argument('--graph_model_dir', type=str)
+    parser.add_argument('--input_pretrained_graph_models_dir', type=str)
+    parser.add_argument('--bert_initialization_model', type=str)
     parser.add_argument('--output_dir', type=str)
     arguments = parser.parse_args()
 
--- sh_scripts/extract_encoder_from_graph_model/extract_encoder_from_graph_model.sh
+++ sh_scripts/extract_encoder_from_graph_model/extract_encoder_from_graph_model.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=extract_encoder_from_graph_model          # Название задачи
-#SBATCH --error=../../logs/extract_encoder_from_graph_model.err        # Файл для вывода ошибок
-#SBATCH --output=../../logs/extract_encoder_from_graph_model.txt       # Файл для вывода результатов
-#SBATCH --time=00:30:00                      # Максимальное время выполнения
+#SBATCH --error=../../logs/extract_encoder_from_graph_model/extract_encoder_from_graph_model.err        # Файл для вывода ошибок
+#SBATCH --output=../../logs/extract_encoder_from_graph_model/extract_encoder_from_graph_model.txt       # Файл для вывода результатов
+#SBATCH --time=20:30:00                      # Максимальное время выполнения
 #SBATCH --cpus-per-task=3                   # Количество CPU на одну задачу
 #SBATCH --gpus=1
 
-python ../../scripts/postprocessing/extract_encoder_from_graph_model.py --model_checkpoint_path="../../pretrained_models/2020AB/Node2vec/RU_split/nns-c-wpn-p-q-wl_1-1-3.0-2.0-5_b128_lr2e-05/checkpoint_e_19_steps_20360.pth" \
---graph_model_dir="../../pretrained_models/2020AB/Node2vec/RU_split/nns-c-wpn-p-q-wl_1-1-3.0-2.0-5_b128_lr2e-05" \
---output_dir="../../pretrained_encoders/2020AB/Node2vec/RU_split/nns-c-wpn-p-q-wl_1-1-3.0-2.0-5_b128_lr2e-05_checkpoint_e_19_steps_20360/"
-
+python ../../scripts/postprocessing/extract_encoder_from_graph_model.py --input_pretrained_graph_models_dir="../../pretrained_models/2020AB/Node2vec/RU_split/" \
+--bert_initialization_model="../../models/cambridgeltl/SapBERT-UMLS-2020AB-all-lang-from-XLMR/" \
+--output_dir="../../pretrained_encoders/2020AB/Node2vec/RU_split/"
