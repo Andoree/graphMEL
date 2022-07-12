1771eeed1ff21083c097326ee7369481ae848837
Andoree <and2900@inbox.ru>
2022-07-12 16:06:38 +0300

    Siblings lookup script change

--- scripts/statistics/siblings_lookup.py
+++ scripts/statistics/siblings_lookup.py
@@ -8,7 +8,7 @@ from typing import List, Tuple, Set, Dict
 import pandas as pd
 from tqdm import tqdm
 
-from graphmel.scripts.utils.io import read_mrrel
+from graphmel.scripts.utils.io import read_mrrel, read_mrconso
 
 
 def get_concept_relation_triplet_strings(mrrel_df: pd.DataFrame) -> Set[str]:
@@ -46,48 +46,67 @@ def count_childs(adjacency_lists_dict: Dict[str, Set[str]]) -> int:
     return count
 
 
-def count_interchild_parent_relations(adjacency_lists_dict: Dict[str, Set[str]]):
-    num_relations = 0
-    for cui_1, child_cuis_list in tqdm(adjacency_lists_dict.items(), total=len(adjacency_lists_dict.keys()),
-                                       miniters=len(adjacency_lists_dict.keys()) // 100):
-        num_siblings = len(child_cuis_list)
-        if num_siblings > 1:
-            assert len(list(itertools.combinations(child_cuis_list, 2))) == int(num_siblings * (num_siblings - 1) / 2)
-            for (sibling_cui_1, sibling_cui_2) in itertools.combinations(child_cuis_list, 2):
-                if adjacency_lists_dict.get(sibling_cui_1) is not None:
-                    if sibling_cui_2 in adjacency_lists_dict[sibling_cui_1]:
-                        print(f"cui_1: {cui_1}, sibling_cui_1: {sibling_cui_1}, sibling_cui_2: {sibling_cui_2}")
-                        num_relations += 1
-                if adjacency_lists_dict.get(sibling_cui_2) is not None:
-                    if sibling_cui_1 in adjacency_lists_dict[sibling_cui_2]:
-                        print(f"cui_1: {cui_1}, sibling_cui_2: {sibling_cui_2}, sibling_cui_1: {sibling_cui_1}")
-                        num_relations += 1
-                # assert not (sibling_cui_2 in adjacency_lists_dict[sibling_cui_1]
-                #             and sibling_cui_1 in adjacency_lists_dict[sibling_cui_2])
-    return num_relations
+def write_intersiblings_hierarhical_relations(adjacency_lists_dict: Dict[str, Set[str]], cui2term: Dict[str, str],
+                                              output_path: str):
+    with codecs.open(output_path, 'w+', encoding="utf-8") as out_file:
+        out_file.write(f"common_parent||parent_sibling||child_sibling\n")
+        for cui_1, child_cuis_list in tqdm(adjacency_lists_dict.items(), total=len(adjacency_lists_dict.keys()),
+                                           miniters=len(adjacency_lists_dict.keys()) // 100):
+            num_siblings = len(child_cuis_list)
+            if num_siblings > 1:
+                assert len(list(itertools.combinations(child_cuis_list, 2))) == int(num_siblings * (num_siblings - 1) / 2)
+                for (sibling_cui_1, sibling_cui_2) in itertools.combinations(child_cuis_list, 2):
+                    parent_sibling_cui, child_sibling_cui = None, None
+                    if adjacency_lists_dict.get(sibling_cui_1) is not None:
+                        if sibling_cui_2 in adjacency_lists_dict[sibling_cui_1]:
+                            parent_sibling_cui, child_sibling_cui = sibling_cui_1, sibling_cui_2
+                    if adjacency_lists_dict.get(sibling_cui_2) is not None:
+                        if sibling_cui_1 in adjacency_lists_dict[sibling_cui_2]:
+                            parent_sibling_cui, child_sibling_cui = sibling_cui_2, sibling_cui_1
+                    if parent_sibling_cui is not None and child_sibling_cui is not None:
+                        parent_sibling_str, child_sibling_str = cui2term[parent_sibling_cui], cui2term[child_sibling_cui]
+                        out_file.write(f"{cui_1}||{parent_sibling_str}||{child_sibling_str}\n")
+
+def create_cui2term_dict(mrconso_df: pd.DataFrame) -> Dict[str, str]:
+    cui2term = {}
+    for _, row in tqdm(mrconso_df.iterrows(), total=mrconso_df.shape[0], miniters=mrconso_df.shape[0] // 100):
+        cui = row["CUI"].strip()
+        term = row["STR"].strip()
+        cui2term[cui] = term
+    return cui2term
+
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('--mrrel')
-    parser.add_argument('--output_path')
+    parser.add_argument('--mrconso')
+    parser.add_argument('--ru_output_path')
+    parser.add_argument('--en_output_path')
     args = parser.parse_args()
 
-    output_path = args.output_path
-    output_dir = os.path.dirname(output_path)
+    ru_output_path = args.ru_output_path
+    output_dir = os.path.dirname(ru_output_path)
     if not os.path.exists(output_dir) and output_dir != '':
         os.makedirs(output_dir)
-
+    df_mrconso = read_mrconso(args.mrconso)
+    ru_df_mrconso = df_mrconso[df_mrconso["LAT"] == "RUS"]
+    en_df_mrconso = df_mrconso[df_mrconso["LAT"] == "ENG"]
+    del df_mrconso
+    ru_cui2term = create_cui2term_dict(mrconso_df=ru_df_mrconso)
+    en_cui2term = create_cui2term_dict(mrconso_df=en_df_mrconso)
+    del ru_df_mrconso
+    del en_df_mrconso
     df_mrrel = read_mrrel(args.mrrel)
     triplet_strings_set = get_concept_relation_triplet_strings(mrrel_df=df_mrrel)
     examples = random.sample(triplet_strings_set, 5)
     for e in examples:
         print(e)
     adjacency_lists_dict = create_child_parent_adjacency_lists(triplet_strings_set=triplet_strings_set)
-    num_childs = count_childs(adjacency_lists_dict=adjacency_lists_dict)
-    interchild_parents = count_interchild_parent_relations(adjacency_lists_dict=adjacency_lists_dict)
-    with codecs.open(output_path, 'w+', encoding="utf-8") as out_file:
-        out_file.write(f"Num triplets: {len(triplet_strings_set)}\nNum childs: {num_childs}\nInterchild parents: {interchild_parents}\n")
+    write_intersiblings_hierarhical_relations(adjacency_lists_dict=adjacency_lists_dict, cui2term=ru_cui2term,
+                                              output_path=args.ru_output_path)
+    write_intersiblings_hierarhical_relations(adjacency_lists_dict=adjacency_lists_dict, cui2term=en_cui2term,
+                                              output_path=args.en_output_path)
 
 
 if __name__ == '__main__':
