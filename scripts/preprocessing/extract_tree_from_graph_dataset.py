4f3dc13a02c686c9278c9c18f0c86416330864af
Andoree <and2900@inbox.ru>
2022-07-14 15:05:35 +0300

    Hierarhical tree creation code

--- scripts/preprocessing/extract_tree_from_graph_dataset.py
+++ scripts/preprocessing/extract_tree_from_graph_dataset.py
@@ -1,15 +1,50 @@
-# TODO
-
-
 import logging
 import os.path
 from argparse import ArgumentParser
-
 import random
 
 from graphmel.scripts.preprocessing.reformat_umls_to_graph import create_relations2id_dicts, create_cui2node_id_mapping, \
     create_graph_files
-from graphmel.scripts.utils.io import write_strings, read_mrconso, read_mrrel, load_dict
+from graphmel.scripts.utils.io import write_strings, read_mrconso, read_mrrel, load_dict, load_edges_tuples, \
+    save_adjacency_list
+
+HIERARCHICAL_RELS_LIST = ("CHD", "PAR", "RB", "RN")
+
+
+# TODO
+def create_hierarchy_adjacency_lists(edge_tuples: List[Tuple[int]], id2rel: Dict[int, str]) -> \
+        Tuple[Dict[int, Set[int]], Dict[int, Set[int]]]:
+    """
+    Given a list of graph edges, creates two hierarchical adjacency
+    lists based on hierarcical types of "REL" attributes
+    :param edge_tuples: List of edge tuples. Each tuple contains source and
+    target nodes and relation types ("REL" and "RELA" attributes)
+    :param id2rel: inverse mapping for "REL" -> "REL's id"
+    :return: Two adjacency list structures.
+    The first one contains childs grouped by their parent. The second one returns all parents of a given child.
+    """
+    parent_childs_adjacency_list: Dict[int, Set[int]] = {}
+    child_parents_adjacency_list: Dict[int, Set[int]] = {}
+    for t in tqdm(edge_tuples, ):
+        node_id_1 = edge_tuples[0]
+        node_id_2 = edge_tuples[1]
+        rel_id = edge_tuples[2]
+        rel_verbose = id2rel[rel_id]
+        # TODO: Перепроверить с примерами, что RN, PAR действительно в правильную сторону описывают иерархию
+        if rel in ("CHD", "RN"):
+            parent_node_id, child_node_id = node_id_2, node_id_1
+        elif rel in ("PAR", "RB"):
+            parent_node_id, child_node_id = node_id_1, node_id_2
+        else:
+            continue
+        if parent_childs_adjacency_list.get(parent_node_id) is None:
+            parent_childs_adjacency_list[parent_node_id] = set()
+        if child_parents_adjacency_list.get(child_node_id) is None:
+            child_parents_adjacency_list[child_node_id] = set()
+        parent_childs_adjacency_list[parent_node_id].add(child_node_id)
+        child_parents_adjacency_list[child_node_id].add(parent_node_id)
+
+    return parent_childs_adjacency_list, child_parents_adjacency_list
 
 
 def main(args):
@@ -19,82 +54,28 @@ def main(args):
     if not os.path.exists(output_dir) and output_dir != '':
         os.makedirs(output_dir)
 
-
-    # logging.info("Loading MRCONSO....")
-    # mrconso_df = read_mrconso(args.mrconso)
-    # logging.info(f"MRCONSO is loaded. There are {mrconso_df.shape[0]} rows")
-    #
-    # if args.ontology is not None:
-    #     mrconso_df = mrconso_df[mrconso_df.SAB.isin(args.ontology)]
-    #     last_subdir = '_'.join(args.ontology) + f"_{last_subdir}"
-    # logging.info(f"There are {mrconso_df.shape[0]} MRCONSO rows after ontology filtering")
-    # if args.langs is not None:
-    #     mrconso_df = mrconso_df[mrconso_df.LAT.isin(args.langs)]
-    #     last_subdir = '_'.join(args.langs) + f"_{last_subdir}"
-    # output_dir = os.path.join(output_dir, last_subdir)
-    # if not os.path.exists(output_dir) and output_dir != '':
-    #     os.makedirs(output_dir)
-
-    # logging.info(f"There are {mrconso_df.shape[0]} MRCONSO rows after language filtering")
-    # mrconso_df["STR"].fillna('', inplace=True)
-
-    # logging.info("Loading MRREL....")
-    # mrrel_df = read_mrrel(args.mrrel)
-    # rel2id, rela2id = create_relations2id_dicts(mrrel_df)
-    # cui2node_id = create_cui2node_id_mapping(mrconso_df=mrconso_df)
-
-    # logging.info("Creating graph files")
-    # TODO input_graph_dataset_dir
-    input_node_id2terms_list_path = os.path.join(input_graph_dataset_dir, "node_id2terms_list")
-    input_node_id2cui_path = os.path.join(input_graph_dataset_dir, "id2cui")
     input_edges_path = os.path.join(input_graph_dataset_dir, "edges")
     input_rel2rel_id_path = os.path.join(input_graph_dataset_dir, f"rel2id")
-    input_rela2rela_id_path = os.path.join(input_graph_dataset_dir, f"rela2id")
+
+    output_parent_childs_adjacency_list_path = os.path.join(output_dir, "parent_childs_adjacency_list")
+    output_child_parents_adjacency_list_path = os.path.join(output_dir, "child_parents_adjacency_list")
+
+    rel2id = {rel: int(i) for rel, i in load_dict(input_rel2rel_id_path)}
+    id2rel = {i: rel for rel, i in rel2id.items()}
+    edge_tuples = load_edges_tuples(path=input_edges_path)
+    parent_childs_adjacency_list, child_parents_adjacency_list = \
+        create_hierarchy_adjacency_lists(edge_tuples=edge_tuples, id2rel=id2rel)
+    save_adjacency_list(adjacency_list=parent_childs_adjacency_list,
+                        save_path=output_parent_childs_adjacency_list_path, )
+    save_adjacency_list(adjacency_list=child_parents_adjacency_list,
+                        save_path=output_child_parents_adjacency_list_path, )
 
 
-    create_graph_files(mrconso_df=mrconso_df, mrrel_df=mrrel_df, rel2id=rel2id, rela2id=rela2id,
-                       cui2node_id=cui2node_id,
-                       output_node_id2terms_list_path=output_node_id2terms_list_path,
-                       output_node_id2cui_path=output_node_id2cui_path,
-                       output_edges_path=output_edges_path, output_rel2rel_id_path=output_rel2rel_id_path,
-                       output_rela2rela_id_path=output_rela2rela_id_path, ignore_not_mapped_edges=True, )
-    pos_pairs = generate_positive_pairs(mrconso_df=mrconso_df, mrrel_df=mrrel_df, cui2node_id=cui2node_id)
-    if args.split_val:
-        output_train_pos_pairs_path = os.path.join(output_dir, f"train_pos_pairs")
-        output_val_pos_pairs_path = os.path.join(output_dir, f"val_pos_pairs")
-
-        train_proportion = args.train_proportion
-        num_pos_pairs = len(pos_pairs)
-        # Shuffling positive pairs
-        random.shuffle(pos_pairs)
-        num_train_pos_pairs = int(num_pos_pairs * train_proportion)
-        train_pos_pairs = pos_pairs[:num_train_pos_pairs]
-        val_pos_pairs = pos_pairs[num_train_pos_pairs:]
-        logging.info(f"Positive pairs are split: {len(train_pos_pairs)} in train and {len(val_pos_pairs)} in val")
-        logging.info(f"Train positive pair examples:\n" + '\n'.join(train_pos_pairs[:3]))
-        logging.info(f"Val positive pair examples:\n" + '\n'.join(val_pos_pairs[:3]))
-        write_strings(fpath=output_train_pos_pairs_path, strings_list=train_pos_pairs)
-        write_strings(fpath=output_val_pos_pairs_path, strings_list=val_pos_pairs)
-    else:
-        random.shuffle(pos_pairs)
-        output_pos_pairs_path = os.path.join(output_dir, f"train_pos_pairs")
-        logging.info(f"Positive pair examples:\n" + '\n'.join(pos_pairs[:3]))
-        write_strings(fpath=output_pos_pairs_path, strings_list=pos_pairs)
-
-# TODO: Посмотреть что в мтеодах, а потом их полностью переделать
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S', )
     parser = ArgumentParser()
-    # parser.add_argument('--mrconso', type=str)
-    # parser.add_argument('--mrrel', type=str)
-    # parser.add_argument('--langs', nargs='+', default=None)
-    # parser.add_argument('--split_val', action="store_true")
-    # parser.add_argument('--train_proportion', type=float)
-    # parser.add_argument('--ontology', default=None, nargs='+')
     parser.add_argument('--input_graph_dataset_dir', type=str)
     parser.add_argument('--output_dir', type=str)
     arguments = parser.parse_args()
-
     main(arguments)
-
