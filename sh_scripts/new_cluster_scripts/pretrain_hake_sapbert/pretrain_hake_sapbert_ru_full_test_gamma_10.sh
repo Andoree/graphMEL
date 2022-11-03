513746107c88e3928f83aed20cd73f94a063eda4
Marat Saidov <msaidov@sbdagroup.com>
2022-11-03 17:53:18 +0300

    SH scripts: HAKE scripts minor fix

--- sh_scripts/new_cluster_scripts/pretrain_hake_sapbert/pretrain_hake_sapbert_ru_full_test_gamma_10.sh
+++ sh_scripts/new_cluster_scripts/pretrain_hake_sapbert/pretrain_hake_sapbert_ru_full_test_gamma_10.sh
@@ -6,13 +6,14 @@
 #SBATCH --cpus-per-task=2                   # Количество CPU на одну задачу
 #SBATCH --gpus=2                   # Требуемое количество GPU
 #SBATCH --constraint=type_c|type_b|type_a
+#SBATCH --nodes=1
 
 nvidia-smi
 
 export TOKENIZERS_PARALLELISM=false
 python /home/echernyak/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/train_hake_sapbert.py --train_dir="../../data/umls_graph/2020AB_pos_pairs_datasets/RUS_pos_pairs_russian_SPLIT" \
 --text_encoder="/home/echernyak/graph_entity_linking/huggingface_models/bert-base-multilingual-uncased/" \
---dataloader_num_workers0 \
+--dataloader_num_workers=0 \
 --negative_sample_size 4 \
 --hake_gamma 10. \
 --hake_modulus_weight 1.0 \
