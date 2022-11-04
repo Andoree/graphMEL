5699e22362641ef8f1607a52bc52fcff2330ad72
Marat Saidov <msaidov@sbdagroup.com>
2022-11-04 16:16:26 +0300

    Synchronization with cluster

--- sh_scripts/new_cluster_scripts/pretrain_hake_sapbert/pretrain_hake_xlmr_multilingual_full_10.0.sh
+++ sh_scripts/new_cluster_scripts/pretrain_hake_sapbert/pretrain_hake_xlmr_multilingual_full_10.0.sh
@@ -11,7 +11,7 @@
 nvidia-smi
 
 export TOKENIZERS_PARALLELISM=false
-python /home/echernyak/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/train_hake_sapbert.py --train_dir="/home/echernyak/graph_entity_linking/pos_pairs_graph_data/2020AB/ENG_FRE_GER_SPA_DUT_RUS_MULTILINGUAL_FULL" \
+python /home/echernyak/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/train_hake_sapbert.py --train_dir="/home/echernyak/graph_entity_linking/pos_pairs_graph_data/2020AB/ENG_FRE_GER_SPA_DUT_RUS_MULTILINGUAL_FULL_TREE" \
 --text_encoder="/home/echernyak/graph_entity_linking/huggingface_models/xlm-roberta-base/" \
 --dataloader_num_workers=0 \
 --negative_sample_size 4 \
