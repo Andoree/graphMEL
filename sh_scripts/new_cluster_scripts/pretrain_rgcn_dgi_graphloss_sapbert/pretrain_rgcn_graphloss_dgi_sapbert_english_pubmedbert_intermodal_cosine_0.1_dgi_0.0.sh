b9d76367a5311790513497045a2592a0fbcc68fa
Andoree <and2900@inbox.ru>
2022-11-17 19:32:59 +0300

    SH scripts: Changed PubMedBert checkpoint for RGCN models

--- sh_scripts/new_cluster_scripts/pretrain_rgcn_dgi_graphloss_sapbert/pretrain_rgcn_graphloss_dgi_sapbert_english_pubmedbert_intermodal_cosine_0.1_dgi_0.0.sh
+++ sh_scripts/new_cluster_scripts/pretrain_rgcn_dgi_graphloss_sapbert/pretrain_rgcn_graphloss_dgi_sapbert_english_pubmedbert_intermodal_cosine_0.1_dgi_0.0.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=/home/echernyak/graph_entity_linking/graphmel/logs/pretrain_rgcn_dgi_multilingual_full/dgi_graph_loss_english_pubmedbert_pretrain_rgcn_article_1_layer_cosine_0.1_dgi_0.0.txt       # Файл для вывода результатов
 #SBATCH --time=56:00:59                      # Максимальное время выполнения
 #SBATCH --cpus-per-task=4                   # Количество CPU на одну задачу
-#SBATCH --gpus=4                   # Требуемое количество GPU
+#SBATCH --gpus=2                   # Требуемое количество GPU
 #SBATCH --constraint=type_c|type_b|type_a
 #SBATCH --nodes=1
 
@@ -12,7 +12,7 @@
 nvidia-smi
 # --remove_selfloops \
 python /home/echernyak/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/train_rgcn_dgi_sapbert.py --train_dir="/home/echernyak/graph_entity_linking/pos_pairs_graph_data/2020AB/ENG_ENGLISH_FULL/" \
---text_encoder="/home/echernyak/graph_entity_linking/huggingface_models/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/" \
+--text_encoder="/home/echernyak/graph_entity_linking/huggingface_models/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext/" \
 --dataloader_num_workers=0 \
 --rgcn_num_hidden_channels 256 \
 --rgcn_num_blocks 64 \
