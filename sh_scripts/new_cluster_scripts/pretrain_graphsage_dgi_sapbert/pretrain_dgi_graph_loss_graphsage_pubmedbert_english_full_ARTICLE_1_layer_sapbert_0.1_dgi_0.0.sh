b6975119259ffe68f3cbf00f797127cda4c9511a
Andoree <and2900@inbox.ru>
2022-11-17 19:23:52 +0300

    Sh scripts: changed English PubMedBert checkpoint

--- sh_scripts/new_cluster_scripts/pretrain_graphsage_dgi_sapbert/pretrain_dgi_graph_loss_graphsage_pubmedbert_english_full_ARTICLE_1_layer_sapbert_0.1_dgi_0.0.sh
+++ sh_scripts/new_cluster_scripts/pretrain_graphsage_dgi_sapbert/pretrain_dgi_graph_loss_graphsage_pubmedbert_english_full_ARTICLE_1_layer_sapbert_0.1_dgi_0.0.sh
@@ -3,15 +3,15 @@
 #SBATCH --error=/home/echernyak/graph_entity_linking/graphmel/logs/pretrain_graphsage_dgi_multilingual_full/pubmedbert_english_checkpoint_dgi_graph_loss_pretrain_graphsage_sapbert_checkpoint_article_1_layer_sapbert_0.1_dgi_0.0.err        # Файл для вывода ошибок
 #SBATCH --output=/home/echernyak/graph_entity_linking/graphmel/logs/pretrain_graphsage_dgi_multilingual_full/pubmedbert_english_checkpoint_dgi_graph_loss_pretrain_graphsage_sapbert_checkpoint_article_1_layer_sapbert_0.1_dgi_0.0.txt       # Файл для вывода результатов
 #SBATCH --time=48:59:59                      # Максимальное время выполнения
-#SBATCH --cpus-per-task=2                   # Количество CPU на одну задачу
-#SBATCH --gpus=2                   # Требуемое количество GPU
+#SBATCH --cpus-per-task=4                   # Количество CPU на одну задачу
+#SBATCH --gpus=4                   # Требуемое количество GPU
 #SBATCH --constraint=type_c|type_b|type_a
 #SBATCH --nodes=1
 
 export CUDA_VISIBLE_DEVICES=0,1
 nvidia-smi
 python /home/echernyak/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/train_graphsage_dgi_sapbert.py --train_dir="/home/echernyak/graph_entity_linking/pos_pairs_graph_data/2020AB/ENG_ENGLISH_FULL/" \
---text_encoder="/home/echernyak/graph_entity_linking/huggingface_models/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/" \
+--text_encoder="/home/echernyak/graph_entity_linking/huggingface_models/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext/" \
 --dataloader_num_workers=0 \
 --graphsage_num_outer_layers 1 \
 --graphsage_num_inner_layers 3 \
