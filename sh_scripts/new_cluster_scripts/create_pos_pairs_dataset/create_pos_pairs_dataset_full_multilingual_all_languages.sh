f667d7a69270c6ad2b95c2c0f15457ebafbcebd6
Andoree <and2900@inbox.ru>
2022-11-25 00:04:51 +0300

    Sh scripts: dataset creation for all UMLS languages

---
+++ sh_scripts/new_cluster_scripts/create_pos_pairs_dataset/create_pos_pairs_dataset_full_multilingual_all_languages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=create_pos_pairs          # Название задачи
+#SBATCH --error=/home/echernyak/graph_entity_linking/graphmel/logs/create_pos_pairs_dataset/create_pos_pairs_dataset_full_multilingual_all_languages.err        # Файл для вывода ошибок
+#SBATCH --output=/home/echernyak/graph_entity_linking/graphmel/logs/create_pos_pairs_dataset/create_pos_pairs_dataset_full_multilingual_all_languages.txt       # Файл для вывода результатов
+#SBATCH --time=09:50:00                      # Максимальное время выполнения
+#SBATCH --cpus-per-task=1                   # Количество CPU на одну задачу
+
+python /home/echernyak/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/create_positive_triplets_dataset.py --mrconso "/home/echernyak/graph_entity_linking/UMLS/2020AB/MRCONSO.RRF" \
+--mrrel "/home/echernyak/graph_entity_linking/UMLS/2020AB/MRREL.RRF" \
+--langs "ENG" "SPA" "POR" "FRE" "JPN" "RUS" "DUT" "GER" "ITA" "CZE" "SWE" "KOR" "LAV" "HUN" "CHI" "NOR" "POL" "TUR" "EST" "FIN" "SCR" "UKR" "GRE" "DAN" "BAQ" "HEB" \
+--output_dir "/home/echernyak/graph_entity_linking/pos_pairs_graph_data/2020AB/MULTILINGUAL_ALL_LANGUAGES_FULL/"
+
