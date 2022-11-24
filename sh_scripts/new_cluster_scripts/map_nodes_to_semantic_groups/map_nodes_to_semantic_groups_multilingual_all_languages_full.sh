7e9269376ba9c94692b1ede03c785fb55e940f80
Andoree <and2900@inbox.ru>
2022-11-25 01:29:42 +0300

    Sh scripts: map nodes to semantic groups, all languages

---
+++ sh_scripts/new_cluster_scripts/map_nodes_to_semantic_groups/map_nodes_to_semantic_groups_multilingual_all_languages_full.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=map_sem_groups          # Название задачи
+#SBATCH --error=/home/echernyak/graph_entity_linking/graphmel/logs/map_node_ids2sem_group/map_node_ids2sem_group_multilingual_all_languages_full.err        # Файл для вывода ошибок
+#SBATCH --output=/home/echernyak/graph_entity_linking/graphmel/logs/map_node_ids2sem_group/map_node_ids2sem_group_multilingual_all_languages_full.txt       # Файл для вывода результатов
+#SBATCH --time=05:45:00                      # Максимальное время выполнения
+#SBATCH --cpus-per-task=2                   # Количество CPU на одну задачу
+#SBATCH --reservation=maint
+
+# Multilingual all languages full
+python /home/echernyak/graph_entity_linking/graphmel/graphmel/scripts/preprocessing/map_node_ids2sem_group.py \
+--graph_dataset_dir "/home/echernyak/graph_entity_linking/pos_pairs_graph_data/2020AB/ENG_SPA_POR_FRE_JPN_RUS_DUT_GER_ITA_CZE_SWE_KOR_LAV_HUN_CHI_NOR_POL_TUR_EST_FIN_SCR_UKR_GRE_DAN_BAQ_HEB_MULTILINGUAL_ALL_LANGUAGES_FULL_TREE" \
+--sem_groups_file "/home/echernyak/graph_entity_linking/UMLS/2020AB/SemGroups_2018.txt" \
+--mrsty "/home/echernyak/graph_entity_linking/UMLS/2020AB/MRSTY.RRF"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
