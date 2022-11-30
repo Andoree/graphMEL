854a8b9848794551c5c8025d1bf6d9e4863e0dc9
Andoree <and2900@inbox.ru>
2022-12-01 00:06:34 +0300

    Sh scripts: English models

---
+++ sh_scripts/new_cluster_scripts/pretrain_final_models/english_models/pretrain_graph_loss_GATV2_pubmedbert_english_full_sapbert_0.1_dgi_0.0_nominer_768_5_neighbors.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH --job-name=mu_gat_dg          # Название задачи
+#SBATCH --error=/home/echernyak/graph_entity_linking/graphmel/logs/pretrain_graph_models_final/pretrain_graph_loss_GATV2_pubmedbert_english_full_sapbert_0.1_dgi_0.0_nominer_768_5_neighbors.err        # Файл для вывода ошибок
+#SBATCH --output=/home/echernyak/graph_entity_linking/graphmel/logs/pretrain_graph_models_final/pretrain_graph_loss_GATV2_pubmedbert_english_full_sapbert_0.1_dgi_0.0_nominer_768_5_neighbors.txt       # Файл для вывода результатов
+#SBATCH --time=38:00:59                      # Максимальное время выполнения
+#SBATCH --cpus-per-task=4                   # Количество CPU на одну задачу
+#SBATCH --gpus=3                   # Требуемое количество GPU
+#SBATCH --constraint=type_c|type_b|type_a
+#SBATCH --nodes=1
+
+nvidia-smi
+# --remove_selfloops \
+python /home/echernyak/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/train_gatv2_dgi_sapbert.py --train_dir="/home/echernyak/graph_entity_linking/pos_pairs_graph_data/2020AB/ENG_ENGLISH_FULL/" \
+--text_encoder="/home/echernyak/graph_entity_linking/huggingface_models/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext/" \
+--dataloader_num_workers=0 \
+--gat_num_outer_layers 1 \
+--gat_num_inner_layers 3 \
+--gat_num_hidden_channels 768 \
+--gat_num_neighbors 5 \
+--gat_num_att_heads 2 \
+--gat_dropout_p 0.3 \
+--gat_attention_dropout_p 0.1 \
+--use_rel_or_rela "rel" \
+--graph_loss_weight 0.1 \
+--gat_use_relational_features \
+--dgi_loss_weight 0.0 \
+--remove_selfloops \
+--intermodal_loss_weight 0.1 \
+--modality_distance "sapbert" \
+--max_length=32 \
+--use_cuda \
+--learning_rate=2e-5 \
+--weight_decay=0.01  \
+--batch_size=128 \
+--num_epochs=1 \
+--amp \
+--parallel \
+--random_seed=42 \
+--loss="ms_loss" \
+--use_miner \
+--type_of_triplets "all" \
+--miner_margin 0.2 \
+--agg_mode "cls" \
+--save_every_N_epoch=1 \
+--output_dir="/home/echernyak/graph_entity_linking/results/pretrained_graphsapbert/2020AB/FINAL_MODELS/ENGLISH_GATV2_FULL_PUBMEDBERT_NOMINER"
+
+
+
---
+++ sh_scripts/new_cluster_scripts/pretrain_final_models/english_models/pretrain_graph_loss_graphsage_pubmedbert_english_full_sapbert_0.1_dgi_0.0_nominer_768.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --job-name=mdSAXL         # Название задачи
+#SBATCH --error=/home/echernyak/graph_entity_linking/graphmel/logs/pretrain_graph_models_final/pretrain_dgi_graph_loss_graphsage_pubmedbert_english_full_sapbert_0.1_dgi_0.0_nominer_768.err        # Файл для вывода ошибок
+#SBATCH --output=/home/echernyak/graph_entity_linking/graphmel/logs/pretrain_graph_models_final/pretrain_dgi_graph_loss_graphsage_pubmedbert_english_full_sapbert_0.1_dgi_0.0_nominer_768.txt       # Файл для вывода результатов
+#SBATCH --time=48:59:59                      # Максимальное время выполнения
+#SBATCH --cpus-per-task=4                   # Количество CPU на одну задачу
+#SBATCH --gpus=2                   # Требуемое количество GPU
+#SBATCH --constraint=type_c|type_b|type_a
+#SBATCH --nodes=1
+
+export CUDA_VISIBLE_DEVICES=0,1
+nvidia-smi
+python /home/echernyak/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/train_graphsage_dgi_sapbert.py --train_dir="/home/echernyak/graph_entity_linking/pos_pairs_graph_data/2020AB/ENG_ENGLISH_FULL/" \
+--text_encoder="/home/echernyak/graph_entity_linking/huggingface_models/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext/" \
+--dataloader_num_workers=0 \
+--graphsage_num_outer_layers 1 \
+--graphsage_num_inner_layers 3 \
+--graphsage_num_hidden_channels 768 \
+--graphsage_num_neighbors 3 \
+--graphsage_dropout_p 0.3 \
+--dgi_loss_weight 0.00 \
+--intermodal_loss_weight 0.1 \
+--graph_loss_weight 0.1 \
+--modality_distance "sapbert" \
+--text_loss_weight 1.0 \
+--remove_selfloops \
+--max_length=32 \
+--use_cuda \
+--learning_rate=2e-5 \
+--weight_decay=0.01  \
+--batch_size=128 \
+--num_epochs=1 \
+--parallel \
+--amp \
+--random_seed=42 \
+--loss="ms_loss" \
+--use_miner \
+--type_of_triplets "all" \
+--miner_margin 0.2 \
+--agg_mode "cls" \
+--save_every_N_epoch=1 \
+--output_dir="/home/echernyak/graph_entity_linking/results/pretrained_graphsapbert/2020AB/FINAL_MODELS/ENGLISH_GRAPHSAGE_FULL_PUBMEDBERT_NOMINER"
+
+
