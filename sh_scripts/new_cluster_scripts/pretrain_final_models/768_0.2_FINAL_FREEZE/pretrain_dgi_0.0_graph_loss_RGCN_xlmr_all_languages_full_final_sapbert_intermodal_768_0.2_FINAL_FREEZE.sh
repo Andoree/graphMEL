b6c9f02ad76493466cb4a75d47a8e73fe21c8eb8
Andoree <and2900@inbox.ru>
2022-12-08 16:30:26 +0300

    Sh scripts: pretraining with frozen neighbors

---
+++ sh_scripts/new_cluster_scripts/pretrain_final_models/768_0.2_FINAL_FREEZE/pretrain_dgi_0.0_graph_loss_RGCN_xlmr_all_languages_full_final_sapbert_intermodal_768_0.2_FINAL_FREEZE.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#SBATCH --job-name=mu_rgcn_dg          # Название задачи
+#SBATCH --error=/home/etutubalina/graph_entity_linking/graphmel/logs/pretrain_graph_models_final/pretrain_dgi_0.0_graph_loss_RGCN_xlmr_all_languages_full_final_sapbert_intermodal_768_0.2_FINAL_FREEZE.err        # Файл для вывода ошибок
+#SBATCH --output=/home/etutubalina/graph_entity_linking/graphmel/logs/pretrain_graph_models_final/pretrain_dgi_0.0_graph_loss_RGCN_xlmr_all_languages_full_final_sapbert_intermodal_768_0.2_FINAL_FREEZE.txt       # Файл для вывода результатов
+#SBATCH --time=56:00:59                      # Максимальное время выполнения
+#SBATCH --cpus-per-task=2                   # Количество CPU на одну задачу
+#SBATCH --gpus=2                   # Требуемое количество GPU
+#SBATCH --constraint=type_c|type_b|type_a
+#SBATCH --nodes=1
+
+
+nvidia-smi
+# --remove_selfloops \
+python /home/etutubalina/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/train_rgcn_dgi_sapbert.py --train_dir="/home/etutubalina/graph_entity_linking/pos_pairs_graph_data/2020AB/ENG_SPA_POR_FRE_JPN_RUS_DUT_GER_ITA_CZE_SWE_KOR_LAV_HUN_CHI_NOR_POL_TUR_EST_FIN_SCR_UKR_GRE_DAN_BAQ_HEB_MULTILINGUAL_ALL_LANGUAGES_FULL/" \
+--text_encoder="/home/etutubalina/graph_entity_linking/huggingface_models/xlm-roberta-base/" \
+--dataloader_num_workers=0 \
+--rgcn_num_hidden_channels 768 \
+--rgcn_num_blocks 64 \
+--use_rel_or_rela "rel" \
+--rgcn_use_fast_conv \
+--rgcn_num_neighbors 3 \
+--rgcn_num_outer_layers 1 \
+--rgcn_num_inner_layers 3 \
+--rgcn_dropout_p 0.3 \
+--dgi_loss_weight 0.0 \
+--remove_selfloops \
+--graph_loss_weight 0.1 \
+--intermodal_loss_weight 0.1 \
+--modality_distance "sapbert" \
+--text_loss_weight 1.0 \
+--use_intermodal_miner \
+--intermodal_miner_margin 0.2 \
+--freeze_neighbors \
+--max_length=32 \
+--use_cuda \
+--learning_rate=2e-5 \
+--weight_decay=0.01  \
+--batch_size=128 \
+--num_epochs=1 \
+--amp \
+--parallel \
+--random_seed=42 \
+--loss="ms_loss" \
+--use_miner \
+--type_of_triplets "all" \
+--miner_margin 0.2 \
+--agg_mode "cls" \
+--save_every_N_epoch=1 \
+--output_dir="/home/etutubalina/graph_entity_linking/results/pretrained_graphsapbert/2020AB/768_0.2_FINAL_FREEZE/RGCN_DGI_0.0_MULTILINGUAL"
+
+
+python /home/etutubalina/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/train_rgcn_dgi_sapbert.py --train_dir="/home/etutubalina/graph_entity_linking/pos_pairs_graph_data/2020AB/ENG_SPA_POR_FRE_JPN_RUS_DUT_GER_ITA_CZE_SWE_KOR_LAV_HUN_CHI_NOR_POL_TUR_EST_FIN_SCR_UKR_GRE_DAN_BAQ_HEB_MULTILINGUAL_ALL_LANGUAGES_FULL/" \
+--text_encoder="/home/etutubalina/graph_entity_linking/huggingface_models/xlm-roberta-base/" \
+--dataloader_num_workers=0 \
+--rgcn_num_hidden_channels 768 \
+--rgcn_num_blocks 64 \
+--use_rel_or_rela "rel" \
+--rgcn_use_fast_conv \
+--rgcn_num_neighbors 3 \
+--rgcn_num_outer_layers 1 \
+--rgcn_num_inner_layers 3 \
+--rgcn_dropout_p 0.3 \
+--dgi_loss_weight 0.0 \
+--remove_selfloops \
+--graph_loss_weight 0.1 \
+--intermodal_loss_weight 0.1 \
+--modality_distance "sapbert" \
+--text_loss_weight 1.0 \
+--use_intermodal_miner \
+--intermodal_miner_margin 0.2 \
+--freeze_neighbors \
+--max_length=32 \
+--use_cuda \
+--learning_rate=2e-5 \
+--weight_decay=0.01  \
+--batch_size=256 \
+--num_epochs=1 \
+--amp \
+--parallel \
+--random_seed=42 \
+--loss="ms_loss" \
+--use_miner \
+--type_of_triplets "all" \
+--miner_margin 0.2 \
+--agg_mode "cls" \
+--save_every_N_epoch=1 \
+--output_dir="/home/etutubalina/graph_entity_linking/results/pretrained_graphsapbert/2020AB/768_0.2_FINAL_FREEZE/RGCN_DGI_0.0_MULTILINGUAL"
+
+
