38997505e1bf7fcba87fa4b1455bccc96fde531d
Andoree <and2900@inbox.ru>
2023-01-16 06:22:29 +0300

    Sh scripts: text loss

---
+++ sh_scripts/new_cluster_scripts/pretrain_final_models/768_0.2_FINAL_NEIGHBORS_MS_LOSS/monolingual/pretrain_dgi_0.01_graph_loss_GAT_monolingual_SAPBERT_CKPT_full_TEXT_LOSS_NEIIGHBORS_MS_LOSS.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+#SBATCH --job-name=mu_gat_dg          # Название задачи
+#SBATCH --error=/home/etutubalina/graph_entity_linking/graphmel/logs/pretrain_graph_models_final/pretrain_dgi_0.01_graph_loss_GAT_monolingual_SAPBERT_CKPT_full_TEXT_LOSS_NEIIGHBORS_MS_LOSS.err        # Файл для вывода ошибок
+#SBATCH --output=/home/etutubalina/graph_entity_linking/graphmel/logs/pretrain_graph_models_final/pretrain_dgi_0.01_graph_loss_GAT_monolingual_SAPBERT_CKPT_full_TEXT_LOSS_NEIIGHBORS_MS_LOSS.txt       # Файл для вывода результатов
+#SBATCH --time=37:58:59                      # Максимальное время выполнения
+#SBATCH --cpus-per-task=3                   # Количество CPU на одну задачу
+#SBATCH --gpus=2                   # Требуемое количество GPU
+#SBATCH --constraint=type_c|type_b|type_a
+#SBATCH --nodes=1
+
+nvidia-smi
+# --remove_selfloops \
+python /home/etutubalina/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/train_gatv2_dgi_sapbert.py --train_dir="/home/etutubalina/graph_entity_linking/pos_pairs_graph_data/2020AB/SPA_SPA_FULL/" \
+--text_encoder="/home/etutubalina/graph_entity_linking/huggingface_models/cambridgeltl/SapBERT-UMLS-2020AB-all-lang-from-XLMR/" \
+--dataloader_num_workers=0 \
+--gat_num_outer_layers 1 \
+--gat_num_inner_layers 3 \
+--gat_num_hidden_channels 768 \
+--gat_num_neighbors 3 \
+--gat_num_att_heads 2 \
+--gat_dropout_p 0.3 \
+--gat_attention_dropout_p 0.1 \
+--gat_use_relational_features \
+--use_rel_or_rela "rel" \
+--graph_loss_weight 0.0 \
+--dgi_loss_weight 0.0 \
+--intermodal_loss_weight 0.0 \
+--text_loss_weight 1.0 \
+--modality_distance "sapbert" \
+--use_intermodal_miner \
+--intermodal_miner_margin 0.2 \
+--apply_text_loss_to_all_neighbors \
+--max_length=32 \
+--use_cuda \
+--learning_rate=2e-5 \
+--weight_decay=0.01  \
+--batch_size=128 \
+--num_epochs=2 \
+--amp \
+--parallel \
+--random_seed=42 \
+--loss="ms_loss" \
+--use_miner \
+--type_of_triplets "all" \
+--miner_margin 0.2 \
+--agg_mode "cls" \
+--save_every_N_epoch=1 \
+--output_dir="/home/etutubalina/graph_entity_linking/results/pretrained_graphsapbert/2020AB/768_0.2_FINAL_NEIGHBORS_MS_LOSS_MONOLINGUAL/TEXT_LOSS_NEIGHBORS_MS_SPA"
+
+
+python /home/etutubalina/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/train_gatv2_dgi_sapbert.py --train_dir="/home/etutubalina/graph_entity_linking/pos_pairs_graph_data/2020AB/DUT_DUT_FULL/" \
+--text_encoder="/home/etutubalina/graph_entity_linking/huggingface_models/cambridgeltl/SapBERT-UMLS-2020AB-all-lang-from-XLMR/" \
+--dataloader_num_workers=0 \
+--gat_num_outer_layers 1 \
+--gat_num_inner_layers 3 \
+--gat_num_hidden_channels 768 \
+--gat_num_neighbors 3 \
+--gat_num_att_heads 2 \
+--gat_dropout_p 0.3 \
+--gat_attention_dropout_p 0.1 \
+--gat_use_relational_features \
+--use_rel_or_rela "rel" \
+--graph_loss_weight 0.0 \
+--dgi_loss_weight 0.0 \
+--intermodal_loss_weight 0.0 \
+--text_loss_weight 1.0 \
+--modality_distance "sapbert" \
+--use_intermodal_miner \
+--intermodal_miner_margin 0.2 \
+--apply_text_loss_to_all_neighbors \
+--max_length=32 \
+--use_cuda \
+--learning_rate=2e-5 \
+--weight_decay=0.01  \
+--batch_size=128 \
+--num_epochs=2 \
+--amp \
+--parallel \
+--random_seed=42 \
+--loss="ms_loss" \
+--use_miner \
+--type_of_triplets "all" \
+--miner_margin 0.2 \
+--agg_mode "cls" \
+--save_every_N_epoch=1 \
+--output_dir="/home/etutubalina/graph_entity_linking/results/pretrained_graphsapbert/2020AB/768_0.2_FINAL_NEIGHBORS_MS_LOSS_MONOLINGUAL/TEXT_LOSS_NEIGHBORS_MS_DUT"
+
+python /home/etutubalina/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/train_gatv2_dgi_sapbert.py --train_dir="/home/etutubalina/graph_entity_linking/pos_pairs_graph_data/2020AB/FRE_FRE_FULL/" \
+--text_encoder="/home/etutubalina/graph_entity_linking/huggingface_models/cambridgeltl/SapBERT-UMLS-2020AB-all-lang-from-XLMR/" \
+--dataloader_num_workers=0 \
+--gat_num_outer_layers 1 \
+--gat_num_inner_layers 3 \
+--gat_num_hidden_channels 768 \
+--gat_num_neighbors 3 \
+--gat_num_att_heads 2 \
+--gat_dropout_p 0.3 \
+--gat_attention_dropout_p 0.1 \
+--gat_use_relational_features \
+--use_rel_or_rela "rel" \
+--graph_loss_weight 0.0 \
+--dgi_loss_weight 0.0 \
+--intermodal_loss_weight 0.0 \
+--text_loss_weight 1.0 \
+--modality_distance "sapbert" \
+--use_intermodal_miner \
+--intermodal_miner_margin 0.2 \
+--apply_text_loss_to_all_neighbors \
+--max_length=32 \
+--use_cuda \
+--learning_rate=2e-5 \
+--weight_decay=0.01  \
+--batch_size=128 \
+--num_epochs=2 \
+--amp \
+--parallel \
+--random_seed=42 \
+--loss="ms_loss" \
+--use_miner \
+--type_of_triplets "all" \
+--miner_margin 0.2 \
+--agg_mode "cls" \
+--save_every_N_epoch=1 \
+--output_dir="/home/etutubalina/graph_entity_linking/results/pretrained_graphsapbert/2020AB/768_0.2_FINAL_NEIGHBORS_MS_LOSS_MONOLINGUAL/TEXT_LOSS_NEIGHBORS_MS_FRE"
+
+python /home/etutubalina/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/train_gatv2_dgi_sapbert.py --train_dir="/home/etutubalina/graph_entity_linking/pos_pairs_graph_data/2020AB/GER_GER_FULL/" \
+--text_encoder="/home/etutubalina/graph_entity_linking/huggingface_models/cambridgeltl/SapBERT-UMLS-2020AB-all-lang-from-XLMR/" \
+--dataloader_num_workers=0 \
+--gat_num_outer_layers 1 \
+--gat_num_inner_layers 3 \
+--gat_num_hidden_channels 768 \
+--gat_num_neighbors 3 \
+--gat_num_att_heads 2 \
+--gat_dropout_p 0.3 \
+--gat_attention_dropout_p 0.1 \
+--gat_use_relational_features \
+--use_rel_or_rela "rel" \
+--graph_loss_weight 0.0 \
+--dgi_loss_weight 0.0 \
+--intermodal_loss_weight 0.0 \
+--text_loss_weight 1.0 \
+--modality_distance "sapbert" \
+--use_intermodal_miner \
+--intermodal_miner_margin 0.2 \
+--apply_text_loss_to_all_neighbors \
+--max_length=32 \
+--use_cuda \
+--learning_rate=2e-5 \
+--weight_decay=0.01  \
+--batch_size=128 \
+--num_epochs=2 \
+--amp \
+--parallel \
+--random_seed=42 \
+--loss="ms_loss" \
+--use_miner \
+--type_of_triplets "all" \
+--miner_margin 0.2 \
+--agg_mode "cls" \
+--save_every_N_epoch=1 \
+--output_dir="/home/etutubalina/graph_entity_linking/results/pretrained_graphsapbert/2020AB/768_0.2_FINAL_NEIGHBORS_MS_LOSS_MONOLINGUAL/TEXT_LOSS_NEIGHBORS_MS_GER"
+
+
+
