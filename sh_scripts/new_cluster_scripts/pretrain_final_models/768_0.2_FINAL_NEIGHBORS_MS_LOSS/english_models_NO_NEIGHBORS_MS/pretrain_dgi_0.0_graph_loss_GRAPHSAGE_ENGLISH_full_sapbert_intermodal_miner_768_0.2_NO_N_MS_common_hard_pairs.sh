bf1fd478c716c19106f622904637311a6bdb2104
Zulfat <zulfatmi@gmail.com>
2023-05-18 18:51:14 +0300

    Sh scripts

---
+++ sh_scripts/new_cluster_scripts/pretrain_final_models/768_0.2_FINAL_NEIGHBORS_MS_LOSS/english_models_NO_NEIGHBORS_MS/pretrain_dgi_0.0_graph_loss_GRAPHSAGE_ENGLISH_full_sapbert_intermodal_miner_768_0.2_NO_N_MS_common_hard_pairs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH --job-name=en_sage          # Название задачи
+#SBATCH --error=/home/etutubalina/graph_entity_linking/graphmel/logs/pretrain_graph_models_final/pretrain_dgi_0.0_graph_loss_GRAPHSAGE_ENGLISH_full_sapbert_intermodal_miner_768_0.2_NO_N_MS_common_hard_pairs.err        # Файл для вывода ошибок
+#SBATCH --output=/home/etutubalina/graph_entity_linking/graphmel/logs/pretrain_graph_models_final/pretrain_dgi_0.0_graph_loss_GRAPHSAGE_ENGLISH_full_sapbert_intermodal_miner_768_0.2_NO_N_MS_common_hard_pairs.txt       # Файл для вывода результатов
+#SBATCH --time=22:55:59                      # Максимальное время выполнения
+#SBATCH --cpus-per-task=2                   # Количество CPU на одну задачу
+#SBATCH --gpus=2                   # Требуемое количество GPU
+#SBATCH --constraint=type_c|type_b|type_a
+#SBATCH --nodes=1
+
+export CUDA_VISIBLE_DEVICES=0,1
+nvidia-smi
+python /home/etutubalina/graph_entity_linking/graphmel/graphmel/scripts/self_alignment_pretraining/train_graphsage_dgi_sapbert.py --train_dir="/home/etutubalina/graph_entity_linking/pos_pairs_graph_data/2020AB/ENG_ENGLISH_FULL/" \
+--text_encoder="/home/etutubalina/graph_entity_linking/huggingface_models/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext/" \
+--dataloader_num_workers=0 \
+--graphsage_num_outer_layers 1 \
+--graphsage_num_inner_layers 3 \
+--graphsage_num_hidden_channels 768 \
+--graphsage_num_neighbors 3 \
+--graphsage_dropout_p 0.3 \
+--dgi_loss_weight 0.0 \
+--intermodal_loss_weight 0.1 \
+--graph_loss_weight 0.1 \
+--modality_distance "sapbert" \
+--common_hard_pairs \
+--text_loss_weight 1.0 \
+--use_intermodal_miner \
+--intermodal_miner_margin 0.2 \
+--remove_selfloops \
+--max_length=32 \
+--use_cuda \
+--learning_rate=2e-5 \
+--weight_decay=0.01  \
+--batch_size=128 \
+--num_epochs=1 \
+--parallel \
+--amp \
+--random_seed=42 \
+--loss="ms_loss" \
+--use_miner \
+--type_of_triplets "all" \
+--miner_margin 0.2 \
+--agg_mode "cls" \
+--save_every_N_epoch=1 \
+--output_dir="/home/etutubalina/graph_entity_linking/results/pretrained_graphsapbert/2020AB/768_0.2_ENGLISH/GRAPHSAGE_DGI_MULTILINGUAL"
+
+
