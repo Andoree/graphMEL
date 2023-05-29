c706c83bb9d5fb9714ade08bb554dff467e38d1e
Andoree <and2900@inbox.ru>
2023-05-29 13:44:33 +0300

    Stats fix

--- graphmel/scripts/preprocessing/count_num_unique_cui_str_pairs.py
+++ graphmel/scripts/preprocessing/count_num_unique_cui_str_pairs.py
@@ -22,15 +22,15 @@ def main():
     df_mrconso_unique_cui_str_lat = df_mrconso.drop_duplicates(subset=("CUI", "STR", "LAT"), inplace=False)
     logging.info(f"Drooped non-unique <CUI, STR, LAT> MRCONSO: {df_mrconso_unique_cui_str_lat.shape}")
 
-    gb_cui_str_lat_df = df_mrconso_unique_cui_str_lat.groupby(['CUI', 'STR', 'LAT'])['STR'].count()
+    gb_cui_str_lat_df = df_mrconso_unique_cui_str_lat.groupby(['CUI', 'STR', 'LAT'])[['CUI', 'STR']].count()
     gb_cui_str_lat_path = os.path.join(output_dir, "gb_cui_str_lat.tsv")
-    gb_cui_str_lat_df.to_csv(gb_cui_str_lat_path, sep='\t', index=False)
+    gb_cui_str_lat_df.to_csv(gb_cui_str_lat_path, sep='\t',)
 
     df_mrconso_unique_cui_str = df_mrconso[["CUI", "STR"]].drop_duplicates(subset=("CUI", "STR"), inplace=False)
 
-    gb_cui_str_df = df_mrconso_unique_cui_str.groupby(['CUI', 'STR'])['STR'].count()
+    gb_cui_str_df = df_mrconso_unique_cui_str.groupby(['CUI', 'STR']).count()
     gb_cui_str_path = os.path.join(output_dir, "gb_cui_str.tsv")
-    gb_cui_str_df.to_csv(gb_cui_str_path, sep='\t', index=False)
+    gb_cui_str_df.to_csv(gb_cui_str_path, sep='\t', )
 
 
 
