12b96a88c74371eec781dc8dfd7665c4753de3b9
Zulfat <zulfatmi@gmail.com>
2023-05-31 15:55:14 +0300

    Sh scripts: changed language limits

--- sh_scripts/new_cluster_scripts/create_pos_pairs_dataset/create_pos_pairs_dataset_full_multilingual_all_languages_stratified_fixed_limit_eng_10_100.sh
+++ sh_scripts/new_cluster_scripts/create_pos_pairs_dataset/create_pos_pairs_dataset_full_multilingual_all_languages_stratified_fixed_limit_eng_10_100.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=create_pos_pairs          # Название задачи
-#SBATCH --error=/home/etutubalina/graph_entity_linking/graphmel/logs/create_pos_pairs_dataset/create_pos_pairs_dataset_full_multilingual_all_languages_stratified_fixed_limit_eng_10_50.err        # Файл для вывода ошибок
-#SBATCH --output=/home/etutubalina/graph_entity_linking/graphmel/logs/create_pos_pairs_dataset/create_pos_pairs_dataset_full_multilingual_all_languages_stratified_fixed_limit_eng_10_50.txt       # Файл для вывода результатов
+#SBATCH --error=/home/etutubalina/graph_entity_linking/graphmel/logs/create_pos_pairs_dataset/create_pos_pairs_dataset_full_multilingual_all_languages_stratified_fixed_limit_eng_10_100.err        # Файл для вывода ошибок
+#SBATCH --output=/home/etutubalina/graph_entity_linking/graphmel/logs/create_pos_pairs_dataset/create_pos_pairs_dataset_full_multilingual_all_languages_stratified_fixed_limit_eng_10_100.txt       # Файл для вывода результатов
 #SBATCH --time=19:50:00                      # Максимальное время выполнения
 #SBATCH --cpus-per-task=2                   # Количество CPU на одну задачу
 #SBATCH --constraint=type_b
@@ -10,7 +10,7 @@ python /home/etutubalina/graph_entity_linking/graphmel/graphmel/scripts/preproce
 --mrrel "/home/etutubalina/graph_entity_linking/UMLS/2020AB/MRREL.RRF" \
 --langs "ENG" "SPA" "POR" "FRE" "JPN" "RUS" "DUT" "GER" "ITA" "CZE" "SWE" "KOR" "LAV" "HUN" "CHI" "NOR" "POL" "TUR" "EST" "FIN" "SCR" "UKR" "GRE" "DAN" "BAQ" "HEB" \
 --max_pairs_per_single_lang 10 \
---max_pairs_crosslingual 50 \
+--max_pairs_crosslingual 100 \
 --limit_english_only \
---output_dir "/home/etutubalina/graph_entity_linking/pos_pairs_graph_data/2020AB/MULTILINGUAL_ALL_LANGUAGES_FIXED_LIMIT_ENG_10_50_FULL/"
+--output_dir "/home/etutubalina/graph_entity_linking/pos_pairs_graph_data/2020AB/MULTILINGUAL_ALL_LANGUAGES_FIXED_LIMIT_ENG_10_100_FULL/"
 
