9b6f79013a7d7cb5c9da121ffeff62dc92591cc4
Andoree <and2900@inbox.ru>
2023-10-12 00:34:10 +0300

    Sh: codiesp-d eval

---
+++ sh_scripts/new_cluster_scripts/fair_evaluation/CODIESP-D_mBERGAMOT/fair_eval_RGCN_mBERGAMOT_codiesp-d.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=fair_ev          # Название задачи
+#SBATCH --error=/home/etutubalina/graph_entity_linking/graphmel/logs/fair_evaluation/fair_eval_RGCN_mBERGAMOT_codiesp-d.err        # Файл для вывода ошибок
+#SBATCH --output=/home/etutubalina/graph_entity_linking/graphmel/logs/fair_evaluation/fair_eval_RGCN_mBERGAMOT_codiesp-d.txt       # Файл для вывода результатов
+#SBATCH --time=23:30:00                      # Максимальное время выполнения
+#SBATCH --cpus-per-task=4                   # Количество CPU на одну задачу
+#SBATCH --gpus=1
+
+export CUDA_VISIBLE_DEVICES=0
+MODEL="/home/etutubalina/graph_entity_linking/huggingface_models/BERGAMOT/RGCN-mBERGAMOT/"
+
+echo "codiesp-d BOTH FULL"
+python /home/etutubalina/graph_entity_linking/Fair-Evaluation-BERT/eval_bert_ranking.py --model_dir $MODEL \
+    --data_folder "/home/etutubalina/graph_entity_linking/data_medical_crossing/datasets/codiesp/DIAGNOSTICO/test" \
+    --vocab "/home/etutubalina/graph_entity_linking/data_medical_crossing/vocabs/codiesp-d-codes-es.txt"
+
+
+echo "codiesp-d BOTH-fair_exact_vocab"
+python /home/etutubalina/graph_entity_linking/Fair-Evaluation-BERT/eval_bert_ranking.py --model_dir $MODEL \
+    --data_folder "/home/etutubalina/graph_entity_linking/data_medical_crossing/datasets/codiesp/DIAGNOSTICO/test-fair_exact_vocab/" \
+    --vocab "/home/etutubalina/graph_entity_linking/data_medical_crossing/vocabs/codiesp-d-codes-es.txt"
+
+echo "codiesp-d BOTH-fair_levenshtein_0.2"
+python /home/etutubalina/graph_entity_linking/Fair-Evaluation-BERT/eval_bert_ranking.py --model_dir $MODEL \
+    --data_folder "/home/etutubalina/graph_entity_linking/data_medical_crossing/datasets/codiesp/DIAGNOSTICO/test-fair_levenshtein_0.2/" \
+    --vocab "/home/etutubalina/graph_entity_linking/data_medical_crossing/vocabs/codiesp-d-codes-es.txt"
+
+
+
