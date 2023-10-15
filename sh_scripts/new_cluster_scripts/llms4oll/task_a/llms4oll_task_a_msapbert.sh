749b2bc53f0f8dc94c41440652cb0598cbfbaf46
Andoree <and2900@inbox.ru>
2023-10-15 03:00:51 +0300

    Sh: LLMS4OLL task 1

---
+++ sh_scripts/new_cluster_scripts/llms4oll/task_a/llms4oll_task_a_msapbert.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=fair_ev          # Название задачи
+#SBATCH --error=/home/etutubalina/graph_entity_linking/graphmel/logs/llms4oll/task_a/llms4oll_task_a_msapbert.err        # Файл для вывода ошибок
+#SBATCH --output=/home/etutubalina/graph_entity_linking/graphmel/logs/llms4oll/task_a/llms4oll_task_a_msapbert.txt       # Файл для вывода результатов
+#SBATCH --time=23:50:00                      # Максимальное время выполнения
+#SBATCH --cpus-per-task=4                   # Количество CPU на одну задачу
+#SBATCH --gpus=1
+
+#!/bin/bash
+
+label="nofinetuning"
+device="cuda:0"
+datasets=("nci" "snomedct_us" "medcin")
+templates=("template-1" "template-2" "template-3" "template-4" "template-5" "template-6" "template-7" "template-8")
+models=("/home/etutubalina/graph_entity_linking/huggingface_models/cambridgeltl/SapBERT-UMLS-2020AB-all-lang-from-XLMR/")
+
+for kb_name in "${datasets[@]}"; do
+  index=1
+  for model_name in  "${models[@]}"; do
+    log="results/$kb_name/$index-$kb_name-$model_name.$label.test.log.txt"
+    exec > $log
+    for template in "${templates[@]}"; do
+      echo "Running on dataset: $kb_name , model: $model_name, template: $template!"
+      python3 test.py --kb_name=$kb_name --model_name=$model_name --template=$template --device=$device
+      echo "Inference for  $model_name on template: $template  is done"
+      echo "-----------------------------------------------------------"
+    done
+    index=$((index+1))
+  done
+done
+
+
